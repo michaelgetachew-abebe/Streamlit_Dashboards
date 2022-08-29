56075f3e307f4819f22fd5a4a9f34b2cbb5040a0
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-29 10:43:46 +0300

    Feat(add): added some plots on the dashboard for the first time

--- stock_price_dashboard/stock_price_dashboard.py
+++ stock_price_dashboard/stock_price_dashboard.py
@@ -11,4 +11,12 @@ st.write("""
 Shown are the stock closing price and volume of Google!!! 
 
 """)
+#define a ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on the ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical data for this ticker
+ticker_df = tickerData.history(period="1d", start="2011-7-31", end = '2021-12-29')
 
+st.line_chart(ticker_df.Close)
+st.line_Chart(ticker_df.Volume)
