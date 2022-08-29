8233ce5f4b6825f23cbac54c5373997da97b844c
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-29 15:38:48 +0300

    Feat(add): added the necessary packages for the project

---
+++ eda_basketball_data/eda_basketball_data.py
@@ -0,0 +1,12 @@
+#Import the necessary packages for the dashboard
+
+import streamlit as st
+import pandas as pd
+import base64
+from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as sns
+
+
+
