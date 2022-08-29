5cb52bd641712c0a1f762ce9f99ceaae277f280a
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-29 10:36:46 +0300

    Feat(started): started working on stock price dashboard

---
+++ stock_price_dashboard/stock_price_dashboard.py
@@ -0,0 +1,14 @@
+# Import the core packages required for the project
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.title("STOCK PRICE App")
+st.header("GOOGLE")
+st.write("""
+# Stock Price Appp
+
+Shown are the stock closing price and volume of Google!!! 
+
+""")
+
