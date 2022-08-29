4ef2a11214268581aff80347c011f95a889c5a67
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-29 14:45:19 +0300

    Feat(add): added the some the first two display sections on the dashboard

--- bioinformatics_DNA_count/bioinformatics_DNA.py
+++ bioinformatics_DNA_count/bioinformatics_DNA.py
@@ -1,5 +1,7 @@
 # Import the packages to be used in this script
 
+from asyncore import write
+from itertools import count
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -23,6 +25,43 @@ This app counts the nucleotide composition od query DNA!!!
 # This box queries the user to enter the DNA sequence
 st.header("Enter DNA sequence?")
 
-input_sequence = ">>DNA QUERY\nGAACACGTGGAG ..."
+input_sequence = ">>DNA QUERY\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGGTGCTCGGG\nTCGGGCAAACAGGAAGGTGAAGAAGTATCCTATCAGGACGGAAGGTACGTCCTGGTGCGGAACAACT\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGGTGCTCGGG"
 
-sequence = st.text_area("Sequence input", input_sequence, height = 300)
+input_sequence = st.text_area("Sequence input", input_sequence, height = 300)
+input_sequence = input_sequence.splitlines()
+input_sequence = input_sequence[1:] # Remove the first line of the sequence ">>DNA QUERY" since it isn't part of the sequence!!!
+input_sequence = ''.join(input_sequence) # Concatenate our the list sequence into a single string
+
+st.header("INPUT")
+input_sequence
+
+st.header("OUTPUT")
+st.subheader("1. Print Dictionary")
+
+def DNA_nucleotide_count(seq):
+  i = dict([
+            ('A',seq.count('A')),
+            ('T',seq.count('T')),
+            ('G',seq.count('G')),
+            ('C',seq.count('C'))
+            ])
+  return i
+
+nucleotide_count = DNA_nucleotide_count(input_sequence)
+
+st.write(nucleotide_count)
+
+# Print a textual result of the nucleotide count
+
+st.subheader("2, Print text")
+A_count = f"There are {nucleotide_count['A']} adenine(A)"
+T_count = f"There are {nucleotide_count['T']} thymine(T)"
+G_count = f"There are {nucleotide_count['G']} guanine(G)"
+C_count = f"There are {nucleotide_count['C']} cytosine(C)"
+st.write(A_count)
+st.write(T_count)
+st.write(G_count)
+st.write(C_count)
+
+# Display the dataframe
+
