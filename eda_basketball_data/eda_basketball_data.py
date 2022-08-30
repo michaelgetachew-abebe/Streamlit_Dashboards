f950f3d247e363bab8e853cf29dcc1f5c478c150
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-30 03:51:01 +0300

    Feat(completed) the eda basic project with a little bit of webscrapping

--- eda_basketball_data/eda_basketball_data.py
+++ eda_basketball_data/eda_basketball_data.py
@@ -21,7 +21,7 @@ This app performs simple webscrapping of NBA player stat dataset
 """)
 
 #Web scraping of player stats
-@st.cache
+@st.cache # Improves the user engagement in the model
 def load_data(year):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     html = pd.read_html(url, header = 0)
@@ -39,3 +39,27 @@ playerstats = load_data(selected_year)
 #Team selcetion
 team_list = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team',team_list, team_list)
+
+#Player position selection
+position_list = ['C','PF','SF','PG','SG']
+selected_position = st.sidebar.multiselect('Position', position_list, position_list)
+
+#Data Filtering
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_position))]
+st.header('Display PLayer Stats of Selectd Team(s)')
+st.write('Data Dimensions: ' + str(df_selected_team.shape[0]) + 'rows and ' + str(df_selected_team.shape[1]))
+st.dataframe(df_selected_team)
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
