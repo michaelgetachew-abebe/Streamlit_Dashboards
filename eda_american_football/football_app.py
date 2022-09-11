089a63f8502fa409942f585a283253fe8e2760a7
michaelgetachew <mikygetyos@gmail.com>
2022-09-11 14:13:07 +0300

    Feat(add): added images to the home page of the dashboard

--- eda_american_football/football_app.py
+++ eda_american_football/football_app.py
@@ -4,5 +4,11 @@ import base64
 import matplotlib.pyplot as pd
 import seaborn as sns
 import numpy as np
+from PIL import Image
 
+# Import the Logo image for NFL
+
+image = Image.open('logo.jpg')
 st.title("NFL DASHBOARD")
+st.subheader("NFL Football Stats (Rushing) Explorer")
+st.image(image, use_column_width=True)
